73f5de6dd1744d7e8c44b94d4dc3526336a8b73d
Reid Spencer <reid@reactific.com>
2015-06-29 22:36:05 -0400

    Several Improvements: * Make the output file and cache file hae the same base directory name * Refactor a few things in BuildInfo to separate functions * Add support for builtAtString and builtAtMillis values as an option (BuildTime) * Generate the toString function on multiple lines * Improve the BuildInfoKey descriptions.

--- src/main/scala/sbtbuildinfo/BuildInfoKeys.scala
+++ src/main/scala/sbtbuildinfo/BuildInfoKeys.scala
@@ -4,11 +4,11 @@ import sbt._
 import Keys._
 
 trait BuildInfoKeys {
-  lazy val buildInfo        = taskKey[Seq[File]]("Generates build info.")
-  lazy val buildInfoObject  = settingKey[String]("The name for the generated object.")
-  lazy val buildInfoPackage = settingKey[String]("The name for teh generated package.") 
-  lazy val buildInfoKeys    = settingKey[Seq[BuildInfoKey.Entry[_]]]("Entries for build info.")
+  lazy val buildInfo            = taskKey[Seq[File]]("The task that generates the build info.")
+  lazy val buildInfoObject      = settingKey[String]("The name for the generated object.")
+  lazy val buildInfoPackage     = settingKey[String]("The name for the generated package.")
+  lazy val buildInfoKeys        = settingKey[Seq[BuildInfoKey.Entry[_]]]("Entries for build info.")
   lazy val buildInfoBuildNumber = taskKey[Int]("The build number.")
-  lazy val buildInfoOptions = settingKey[Seq[BuildInfoOption]]("Options to generate build info.")
+  lazy val buildInfoOptions     = settingKey[Seq[BuildInfoOption]]("Options for generating the build info.")
 }
 object BuildInfoKeys extends BuildInfoKeys {}
