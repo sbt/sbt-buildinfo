c700f822d15956ada3ca9ec2b02ec40e574502a8
Akos Vandra <akos@vandra.hu>
2017-01-06 12:27:25 +0100

    add option to use other renderer easily

--- src/main/scala/sbtbuildinfo/BuildInfoRenderer.scala
+++ src/main/scala/sbtbuildinfo/BuildInfoRenderer.scala
@@ -1,12 +1,14 @@
 package sbtbuildinfo
 
+object BuildInfoRenderer {
+  type Factory = (Seq[BuildInfoOption], String, String) => BuildInfoRenderer
+}
+
 trait BuildInfoRenderer {
 
   def fileType: BuildInfoType
   def extension: String
-  def header: Seq[String]
   def renderKeys(infoKeysNameAndValues: Seq[BuildInfoResult]): Seq[String]
-  def footer: Seq[String]
 
   def isSource = fileType == BuildInfoType.Source
   def isResource = fileType == BuildInfoType.Resource
