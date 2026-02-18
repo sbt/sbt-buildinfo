6360a0b329729696fa4f3e123adba1f53313af71
xuwei-k <6b656e6a69@gmail.com>
2026-02-19 08:06:14 +0900

    Avoid deprecated scala.App

--- src/sbt-test/sbt-buildinfo/options/src/main/scala/hello/Test.scala
+++ src/sbt-test/sbt-buildinfo/options/src/main/scala/hello/Test.scala
@@ -2,8 +2,10 @@ package hello
 
 import scala.Predef._
 
-object Test extends scala.App {
-  val expected = """{"name":"helloworld", "scalaVersion":"2.12.21"}"""
-  val actual = hello.BuildInfo.toJson
-  assert(actual == expected, "expected " + expected + " but found " + actual)
+object Test {
+  def main(args: scala.Array[String]): scala.Unit = {
+    val expected = """{"name":"helloworld", "scalaVersion":"2.12.21"}"""
+    val actual = hello.BuildInfo.toJson
+    assert(actual == expected, "expected " + expected + " but found " + actual)
+  }
 }
